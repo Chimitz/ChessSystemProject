b6719289ecc72267f762b8645de0ccf66b67c69e
Kevin de Lima <klimadeoliveira0492@gmail.com>
2024-03-05 17:24:40 -0300

    First class: Position

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Start");
+        Position position = new Position(2, 3);
+        System.out.println(position);
 
     }
 }
